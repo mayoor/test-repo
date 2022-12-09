fbfccdefb3879cd659f52f0d31aa607df73efd42
Mayoor Rao <mayoor.rao@oracle.com>
2022-12-09 08:25:46 -0800

    testing

---
+++ mr_test/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mr-test",
+    version="1.0.0",
+    description="A sample Python package",
+    author="chatgpt",
+    packages=["hello"],
+    install_requires=["numpy>=1.15.0"],
+)
